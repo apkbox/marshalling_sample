cfac9e678237d9d5ada344df606870c43d079104
apkbox <ohnegesicht@gmail.com>
2014-05-10 00:49:03 -0700

    Template based tuple serializer.

--- marshalling_sample/marshalling_sample.cpp
+++ marshalling_sample/marshalling_sample.cpp
@@ -1,5 +1,8 @@
+#include <iostream>
+#include <sstream>
 #include <string>
 #include <tuple>
+#include <utility>
 
 template <int... X_>
 struct sequence {};
@@ -11,6 +14,51 @@ template <int... Y_>
 struct sequence_generator<0, Y_...> {
   using type = sequence<Y_...>;
 };
+
+struct EmptyBase {};
+
+template<typename Base_ = EmptyBase>
+struct BasicSerializerT : public Base_ {
+  template<typename T_>
+  void SerializeValue(const T_ &v, std::string *out) {
+    std::ostringstream ss;
+    ss << v << std::endl;
+    out->append(ss.str());
+  }
+};
+
+template <typename Tuple_, typename Serializer_, int N_, int... Y_>
+struct DataPackerBase : DataPackerBase<Tuple_, Serializer_, N_ - 1, N_ - 1, Y_...> {
+  explicit DataPackerBase(Serializer_ &s) : DataPackerBase<Tuple_, Serializer_, N_ - 1, N_ - 1, Y_...>(s)
+  {
+  }
+
+  virtual void Serialize(const Tuple_ &a, std::string *out) {
+    DataPackerBase<Tuple_, Serializer_, N_ - 1, N_ - 1, Y_...>::Serialize(a, out);
+    serializer_.SerializeValue(std::get<N_ - 1>(a), out);
+  }
+};
+
+template <typename Tuple_, typename Serializer_, int... Y_>
+struct DataPackerBase<Tuple_, Serializer_, 0, Y_...> {
+  explicit DataPackerBase(Serializer_ &s) : serializer_(s)
+  {
+  }
+
+  virtual void Serialize(const Tuple_ &a, std::string *out) {
+    // std::cout << "last" << std::endl;
+  }
+
+  Serializer_ serializer_;
+};
+
+template<typename Tuple_, typename Serializer_ = BasicSerializer>
+std::string SerializeIntoString(const Tuple_ &data, Serializer_ &serializer = BasicSerializerT()) {
+  std::string s;
+  DataPackerBase<Tuple_, Serializer_, std::tuple_size<Tuple_>::value> x(serializer);
+  x.Serialize(data, &s);
+  return s;
+}
 
 struct EventData {
   int data1;
@@ -75,8 +123,51 @@ private:
   }
 };
 
+using EventDataPtr = EventData *;
+
+struct MySerializer {
+#if 0
+  template<typename T_>
+  void SerializeValue(const T_ &v, std::string *out) {
+    std::ostringstream ss;
+    ss << v << std::endl;
+    out->append(ss.str());
+  }
+#endif
+
+  void SerializeValue(const EventDataPtr &v, std::string *out) {
+    std::ostringstream s;
+    s << v->data1 << "," << v->string1 << std::endl;
+    out->append(s.str());
+  }
+};
+#if 0
+  template<>
+void SerializeValue<EventData *>(EventData * const  &v, std::string *out) {
+  std::ostringstream s;
+  s << v->data1 << "," << v->string1 << std::endl;
+  out->append(s.str());
+}
+#endif
+
 int main()
 {
+  //std::tuple<int, long, std::string, double> t = std::make_tuple(1, 43, "aaaaa", 3.14);
+
+  //DataPacker<decltype(t), std::tuple_size<decltype(t)>::value> packer;
+  //packer.Serialize(t);
+
+  EventData d;
+  d.data1 = 43;
+  d.string1 = "Test string";
+
+  BasicSerializerT<MySerializer> ms;
+  std::cout << "-- " << SerializeIntoString(std::make_tuple(), ms) << std::endl;
+  std::cout << "-- " << SerializeIntoString(std::make_tuple(1), ms) << std::endl;
+  std::cout << "-- " << SerializeIntoString(std::make_tuple(1, 2), ms) << std::endl;
+  std::cout << "-- " << SerializeIntoString(std::make_tuple(1, "test"), ms) << std::endl;
+  std::cout << "-- " << SerializeIntoString(std::make_tuple(1, &d), ms) << std::endl;
+
   return 0;
 }
 
